8bc7a4406932b534a43ce00b93e6fd283cbc0a2f
alenchiik <pavlova.alyona.301103@gmail.com>
2025-11-15 22:29:01 +0400

    Полная реализация всех таблиц и связей по ERD

--- corrector/application/database/tables.py
+++ corrector/application/database/tables.py
@@ -1,7 +1,7 @@
 import json
 from datetime import date, time
 from decimal import Decimal
-from sqlalchemy import MetaData, ForeignKey, BigInteger
+from sqlalchemy import MetaData, ForeignKey, BigInteger, Boolean, Integer, Date, Time, Numeric
 from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 
 
@@ -24,93 +24,122 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, comment="Идентификатор")
     first_name: Mapped[str] = mapped_column(nullable=False, comment="Имя")
-    surname_name: Mapped[str] = mapped_column(nullable=False, comment="Фамилия")
-    patronomic_name: Mapped[str] = mapped_column(nullable=False, comment="Отчество")
+    surname_name: Mapped[str] = mapped_column(nullable=True, comment="Фамилия")
+    patronomic_name: Mapped[str] = mapped_column(nullable=True, comment="Отчество")
     user_name: Mapped[str] = mapped_column(unique=True, nullable=False, comment="Логин")
     password: Mapped[str] = mapped_column(nullable=False, comment="Пароль")
-    #tg_username: Mapped[str] = mapped_column(nullable=True, comment="TG") # нужно ли и можно ли
-    #is_tg_subscribed:Mapped[bool] = mapped_column(nullable=True, default = False, comment="Есть ли подписка на канал")
+    tg_username: Mapped[str] = mapped_column(unique=True, nullable=True, comment="Telegram:")
+    is_tg_subscribed: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False,
+        comment="Проверка подписки на Telegram"
+    )
+    is_admin: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False,
+        comment="Проверка на админа"
+    )
+    theme: Mapped[str]=mapped_column(nullable=False, comment="Тема приложения Dark/Light")
+    notification_push: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False,
+        comment="Уведомления в браузере on/off"
+    )
 
-# class Status(Base):
-#     __tablename__ = "status"
-#     __table_args__ = {"comment": "Статусы"}
-
-#     id: Mapped[int] = mapped_column(primary_key=True, comment="Идентификатор")
-#     status_name: Mapped[str] = mapped_column(unique=True, comment="Вид статуса")    
-
-# class Document(Base):
-#     __tablename__ = "documents"
-#     __table_args__ = {"comment": "Документы"}
-
-#     id: Mapped[int] = mapped_column(primary_key=True, comment="Идентификатор")
-#     user_id: Mapped[int] = mapped_column(
-#         ForeignKey("user.id"),
-#         nullable=False, 
-#         comment="Связь с User"
-#     )
-#     status_id: Mapped[int] = mapped_column(
-#         ForeignKey("status.id"),
-#         nullable=False, 
-#         comment="Связь с Status"
-#     )
-#     check_report_id: Mapped[int] = mapped_column(
-#         ForeignKey("check_report.id"),
-#         nullable=False, 
-#         comment="Связь с CheckReports"
-#     )
-#     file_name: Mapped[str] = mapped_column(unique=True,nullable=False, comment="Имя файла")
-#     upload_date: Mapped[date] = mapped_column(nullable=False, comment="Дата загрузки")
-#     upload_time: Mapped[time] = mapped_column(nullable=False, comment="Время загрузки")
-#     size: Mapped[float] = mapped_column(float,nullable=False,comment="Размер файла в МБ")
+class Review(Base):
+    __tablename__= "reviews"
+    __table_args__= {"comment": "Отзывы"}
     
-# class Ratings(Base):
-#     __tablename__ = "rating"
-#     __table_args__ = {"comment": "Оценки"}
-
-#     id: Mapped[int] = mapped_column(primary_key=True, comment="Идентификатор(1-5)")
-#     rating_name: Mapped[str] = mapped_column(unique=True, comment="Вид статуса")
+    id:Mapped[int] = mapped_column(primary_key=True,comment="Индентификатор")
+    user_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("users.id",ondelete="CASCADE"),
+        nullable=False,
+        comment="Связь с User"
+    )
+    mark: Mapped[int] = mapped_column(nullable=False, comment="Оценка от 1 до 5 звезд")
+    review_text: Mapped[str] = mapped_column(nullable=True,comment="Текс отзыва")
+    created_at: Mapped[date] = mapped_column(
+        Date,
+        nullable=False,
+        comment="Время отправки отзыва"
+    )
     
-# class Reviews(Base):
-#     __tablename__ = "review"
-#     __table_args__ = {"comment": "Отзамвы"}
-
-#     id: Mapped[int] = mapped_column(primary_key=True, comment="Идентификатор")
-#     user_id: Mapped[int] = mapped_column(
-#         ForeignKey("user.id"),
-#         nullable=False, 
-#         comment="Связь с User"
-#     )
-#     rating_id:Mapped[int] = mapped_column(
-#         ForeignKey("rating.id"),
-#         nullable=False, 
-#         comment="Связь с Rating"
-#     )
-#     review_text:Mapped[str] = mapped_column(unique=True, comment="Вид статуса")
-#     create_at: Mapped[date] = mapped_column(nullable=False, comment="Дата создания")
-
-# class CheckReports(Base):
-#     __tablename__ = "rating"
-#     __table_args__ = {"comment": "Отчеты о проверке"}
+    class Status(Base):
+        __tablename__= "statuses"
+        __table_args__={"comment": "Статусы"}
+        
+        id: Mapped[int] = mapped_column(primary_key=True,comment="Индентификатор")
+        status_name: Mapped[str] = mapped_column(unique=True, nullable=False, comment="Наименование статуса")
+        
+    class Document(Base):
+        __tablename__= "documents"
+        __table_args__= {"comment": "Документы"}
+        
+        id: Mapped[str] = mapped_column(primary_key=True, comment="Идентификатор")
+        file_name: Mapped[str] = mapped_column (unique=True, nullable="False",comment = "Наименование документа")
+        upload_date: Mapped[date] = mapped_column(
+            Date,
+            nullable="False",
+            comment="Дата загрузки"
+        )
+        upload_time: Mapped[date] = mapped_column(
+            Time,
+            nullable="False",
+            comment="Время загрузки"
+        )
+        size: Mapped[Decimal] = mapped_column(
+            Numeric (precision=5, decimal_return_scale = 2),
+            nullable="False",
+            comment="Размер файла"
+        )
+        user_id: Mapped[int] = mapped_column(
+            ForeignKey("users.id"),
+            nullable="False",
+            comment="Связь с User"
+        )
+        status_id: Mapped[int] = mapped_column(
+            ForeignKey("statuses.id"),
+            nullable = "False",
+            comment= "Связь с Status"
+        )
+        report_pdf_path: Mapped[str] = mapped_column(nullable=False,comment="URL на отчет")
+        score: Mapped[Decimal] = mapped_column(
+            Numeric (precision=3, decimal_return_scale=1),
+            nullable=False,
+            comment="Соответсвие ГОСТу"
+        )
+        analysis_time: Mapped[Decimal] = mapped_column(
+            Numeric(precision=5, decimal_return_scale=2),
+            nullable=False,
+            comment="Время анализа"
+        )
     
-#     id: Mapped[int] = mapped_column(primary_key=True, comment="Идентификатор")
-#     document_id: Mapped[int] = mapped_column(
-#         ForeignKey("document.id"),
-#         nullable=False, 
-#         comment="Связь с Document"
-#     )
-#     check_type_id: Mapped[int] = mapped_column(
-#         ForeignKey("check_report.id"),
-#         nullable=False, 
-#         comment="Связь с CheckTypes"
-#     )
-#     file_type_id: Mapped[int] = mapped_column(
-#         ForeignKey("check_report.id"),
-#         nullable=False, 
-#         comment="Связь с FileTypes"
-#     )
-#     report_date: Mapped[date] = mapped_column(nullable=False, comment="Дата выполнения отчета")
-#     report_pdf_url:Mapped[str] = mapped_column(unique=True, comment="Ссылка на сформированный pdf отчет")
-#     score:Mapped[Decimal] = mapped_column(Decimal(precision=3,scale=1),nullable=False,comment="Общий балл")
-#     analysis_time:Mapped[Decimal] = mapped_column(Decimal(precision=5,scale=2),nullable=False,comment="Время анализа")
-#     overall_compliance:Mapped[Decimal] = mapped_column(Decimal(precision=5,scale=2),nullable=False,comment="% соответствия")
-#     recommendations:Mapped[str] = mapped_column( nulable=True, comment="Рекоменадации")
+    class Mistacke_Type(Base):
+        __tablename__ = "mistake_types"
+        __table_args__={"comment":"Типы ошибок"}
+        
+        id: Mapped[int] = mapped_column(primary_key=True,comment="Индентификатор")
+        mistake_type_name: Mapped[str] = mapped_column(nullable=False, comment="Наименование ошибки")
+        
+    class Mistake(Base):
+        __tablename__ = "mistakes"
+        __table_args__={"comment": "Ошибки"}
+        
+        id: Mapped[int]=mapped_column(primary_key=True,comment="Индентификатор")
+        mistake_type_id:Mapped[int] = mapped_column(
+            ForeignKey("mistake_types.id"),
+            nullable=False,
+            comment="Связь с Mistake_Type"
+        )
+        description: Mapped[str] = mapped_column(nullable=False,comment="Описание ошибки")
+        quantity: Mapped[int] = mapped_column(nullable=False,comment="Общее количество ошибок")
+        critical_status: Mapped[str] = mapped_column(nullable=False,comment="Статус важности")
+        document_id: Mapped[str] = mapped_column(
+            ForeignKey("document.id"),
+            nullable=False,
+            comment="Связь с Document"
+        )
